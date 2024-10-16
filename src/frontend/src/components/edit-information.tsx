cfeb2bb21e7cecb4f9859ed3c1282b3638069877
Kaizouku14 <mandaalv72@gmail.com>
2024-10-16 17:24:56 +0000

    feat: responsive profile and add edit dialog for user information

---
+++ src/frontend/src/components/edit-information.tsx
@@ -0,0 +1,86 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Label } from "@radix-ui/react-label"
+import { UserRoundPen } from "lucide-react"
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+
+const EditDialog = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button className="bg-lime-700 text-white font-medium flex items-center justify-center gap-x-2 px-4 py-2 rounded-lg hover:bg-lime-600 transition-transform transform hover:scale-[1.05] ">
+                    <UserRoundPen className="w-4 h-4 md:w-5 md:h-5" />
+                    <span className="text-sm md:text-base">Edit</span>
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="w-full max-w-[90%] sm:max-w-md p-4 sm:p-6 rounded-lg">
+                <DialogHeader>
+                    <DialogTitle>Edit profile</DialogTitle>
+                    <DialogDescription>
+                        Make changes to your profile here. Click save when you're done.
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="space-y-4">
+                    <div className="flex flex-col">
+                        <Label htmlFor="username" className="text-left mb-2">
+                            Username
+                        </Label>
+                        <Input
+                            id="username"
+                            placeholder="Enter username"
+                            className="w-full"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <Label htmlFor="email-address" className="text-left mb-2">
+                            Email Address
+                        </Label>
+                        <Input
+                            id="email-address"
+                            placeholder="Enter email address"
+                            className="w-full"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <Label htmlFor="country" className="text-left mb-2">
+                            Country
+                        </Label>
+                        <Input
+                            id="country"
+                            placeholder="Enter country"
+                            className="w-full"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <Label htmlFor="city" className="text-left mb-2">
+                            City
+                        </Label>
+                        <Input
+                            id="city"
+                            placeholder="Enter city"
+                            className="w-full"
+                        />
+                    </div>
+                </div>
+                <DialogFooter className="pt-4">
+                    <Button type="submit" className="w-full sm:w-auto">
+                        Save changes
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default EditDialog
--- src/frontend/src/pages/ProfilePage.tsx
+++ src/frontend/src/pages/ProfilePage.tsx
@@ -2,9 +2,10 @@ import Profile from "@/components/avatar";
 import TutorialCard from "@/components/tutorial-card";
 import Footer from "@/layout/Footer";
 import Header from "@/layout/HomeLayout/Header";
-import { Pencil, UserRoundPen } from "lucide-react";
+import { Pencil } from "lucide-react";
 import { FC, useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import EditDialog from "@/components/edit-information";
 
 const ProfilePage: FC = () => {
   const [userProfile, setUserProfile] = useState("https://github.com/shadcn.png");
@@ -23,17 +24,21 @@ const ProfilePage: FC = () => {
   };
 
   return (
-    <div className="mx-[75px] flex flex-col gap-y-4">
+    <div className="md:mx-[75px] flex flex-col gap-y-4 max-md:px-4">
       <Header />
       <div className="flex flex-col gap-y-4">
         <h1 className="text-3xl font-bold text-lime-700">My Profile</h1>
 
-        <div className="border border-gray-200 shadow-lg h-64 rounded-xl bg-white flex items-center gap-x-6 px-6 py-4">
+        <div className="border border-gray-200 shadow-lg md:h-64 h-44 rounded-xl bg-white flex items-center gap-x-6 px-6 py-4">
           <div className="relative">
-            <Profile userProfile={preview || userProfile} style="size-40" />
-            <span className="absolute right-2 bottom-2 bg-slate-300 rounded-full p-2 hover:bg-slate-400 transition-colors duration-150 cursor-pointer">
-              <label htmlFor="profile-upload" aria-label="Change Profile Picture">
-                <Pencil size={15} aria-hidden="true" />
+            <Profile userProfile={preview || userProfile} 
+                     style="size-40 max-md:size-32" />
+            <span 
+                  className="absolute right-2 bottom-2 bg-slate-300 rounded-full p-2 hover:bg-slate-400 transition-colors duration-150 cursor-pointer">
+              <label htmlFor="profile-upload" 
+                     aria-label="Change Profile Picture">
+                <Pencil size={15} 
+                        aria-hidden="true" />
               </label>
               <input
                 id="profile-upload"
@@ -45,53 +50,46 @@ const ProfilePage: FC = () => {
             </span>
           </div>
           <div className="flex flex-col justify-center">
-            <span className="text-2xl font-bold">Gian Carlo Villar</span>
-            <span className="text-slate-600">San Jose Del Monte, Bulacan</span>
+            <span className="text-xl md:text-2xl font-bold">Gian Carlo Villar</span>
+            <span className="text-slate-600 max-md:text-sm">San Jose Del Monte, Bulacan</span>
           </div>
         </div>
 
-        {/* User Information */}
-        <div className="relative border border-gray-200 shadow-lg h-64 rounded-xl p-6 bg-white">
-          <h1 className="text-2xl font-bold text-lime-700 mb-6">User Information</h1>
+        <div className="relative border border-gray-200 shadow-lg md:h-64 h-56 rounded-xl p-6 bg-white">
+          <h1 className="text-xl md:text-2xl font-bold text-lime-700 mb-6">User Information</h1>
 
           <div className="grid grid-cols-2 gap-x-8 items-center">
-            {/* first column */}
             <div className="flex flex-col gap-y-5">
               <div className="flex flex-col">
                 <span className="text-gray-500 text-sm">Full Name</span>
-                <span className="text-lg font-medium text-gray-800">John Doe</span>
+                <span className="text-sm md:text-lg font-medium text-gray-800 max-md:truncate">John Doe</span>
               </div>
               <div className="flex flex-col">
                 <span className="text-gray-500 text-sm">Email Address</span>
-                <span className="text-lg font-medium text-gray-800">Darvey234244@gmail.com</span>
+                <span className="text-sm md:text-lg font-medium text-gray-800 max-md:truncate">Darvey234244@gmail.com</span>
               </div>
             </div>
 
-            {/* second column */}
             <div className="flex flex-col gap-y-5">
               <div className="flex flex-col">
                 <span className="text-gray-500 text-sm">Country</span>
-                <span className="text-lg font-medium text-gray-800">Third World Country</span>
+                <span className="text-sm md:text-lg font-medium text-gray-800">Third World Country</span>
               </div>
               <div className="flex flex-col">
                 <span className="text-gray-500 text-sm">City/State</span>
-                <span className="text-lg font-medium text-gray-800">San Jose Arthur Robes</span>
+                <span className="text-sm md:text-lg font-medium text-gray-800 max-md:truncate">San Jose Arthur Robes</span>
               </div>
             </div>
-
-            {/* Edit Button */}
-            <button className="absolute right-3 top-3 bg-lime-700 text-white font-medium flex items-center justify-center gap-x-2 px-4 py-2 rounded-lg hover:bg-lime-600 transition-transform transform hover:scale-[1.05] focus:ring focus:ring-lime-400">
-              <UserRoundPen className="w-4 h-4" />
-              <span>Edit</span>
-            </button>
+            <div className="absolute right-3 md:top-3 top-6">
+             <EditDialog/>
+            </div>
           </div>
         </div>
 
-        {/* Timeline */}
         <div className="border border-gray-200 shadow-lg h-[30rem] rounded-xl p-6 bg-white">
           <h1 className="text-2xl font-bold text-lime-700 mb-4">Timeline</h1>
           <ScrollArea className="h-96">
-            <div className="grid grid-cols-4 gap-4 px-2.5">
+            <div className="grid grid-col-1 md:grid-cols-4 gap-4 px-2.5">
               {
                 [
                   {
