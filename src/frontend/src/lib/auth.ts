d3eb22aa8d2baea5e88901bbbba874caaa3ed247
xyugen <alex.arias.jorym@gmail.com>
2024-10-17 19:02:50 +0000

    feat(auth): add auth utility functions for token management

---
+++ src/frontend/src/lib/auth.ts
@@ -0,0 +1,39 @@
+import { LocalStorageKeys } from "@/constants/LocalStorageKeys";
+
+export const isAuthenticated = (): boolean => {
+  const token = localStorage.getItem(LocalStorageKeys.AUTH_TOKEN);
+
+  if (!token) {
+    return false
+  }
+
+  try {
+    return true;
+  } catch (error) {
+    localStorage.removeItem("token");
+    return false;
+  }
+}
+
+
+export const decodeAuthToken = (token: string) => {
+  const decodedToken = atob(token);
+
+  const tokenPayload = JSON.parse(decodedToken);
+
+  return tokenPayload;
+};
+
+export const getToken = () => {
+  const token = localStorage.getItem(LocalStorageKeys.AUTH_TOKEN);
+
+  if (!token) {
+    return null
+  }
+
+  return token
+}
+
+export const removeToken = () => {
+  localStorage.removeItem(LocalStorageKeys.AUTH_TOKEN);
+}
