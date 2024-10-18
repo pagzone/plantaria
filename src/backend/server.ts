59ed065624d475f57363e5966cae67bd883062ac
xyugen <alex.arias.jorym@gmail.com>
2024-10-18 07:21:30 +0000

    fix: api endpoint on backend to /icp

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -2,6 +2,7 @@
 import express from "express";
 
 import ExceptionHandler from "./app/Exceptions/Handler";
+import cors from "cors";
 import { routes } from "./start/routes";
 
 export function CreateServer() {
@@ -14,10 +15,10 @@ export function CreateServer() {
 	// 	credentials: true,
 	// }
 
-	// app.use(cors(corsOptions));
 	app.use(express.json({ limit: "1.5mb" }));
+	app.use(cors());
 	app.use(ExceptionHandler);
-	app.use("/api", routes);
+	app.use("/icp", routes);
 	app.use(express.static("/dist"));
 	return app.listen();
 }
--- src/frontend/src/constants/ApiRoutes.ts
+++ src/frontend/src/constants/ApiRoutes.ts
@@ -1,6 +1,6 @@
 export enum APIRoutes {
-	LOGIN = "/api/user/login",
-	REGISTER = "/api/user/register",
-	LOGIN_WITH_IDENTITY = "/api/user/ii/login",
-	REGISTER_WITH_IDENTITY = "/api/user/ii/register",
+	LOGIN = "/icp/user/login",
+	REGISTER = "/icp/user/register",
+	LOGIN_WITH_IDENTITY = "/icp/user/ii/login",
+	REGISTER_WITH_IDENTITY = "/icp/user/ii/register",
 }
