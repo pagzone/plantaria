a45b28ff94baab1827802e8632021d0cc66c0a63
xyugen <alex.arias.jorym@gmail.com>
2024-10-20 13:41:05 +0000

    fix: favorites entity not added in db index

--- src/backend/database/entities/index.ts
+++ src/backend/database/entities/index.ts
@@ -2,5 +2,6 @@ import { Configuration } from "./configuration";
 import { User } from "./user";
 import { Tutorial } from "./tutorial";
 import { Story } from "./story";
+import { Favorite } from "./favorite";
 
-export const ENTITIES = [Configuration, User, Tutorial, Story];
+export const ENTITIES = [Configuration, User, Tutorial, Story, Favorite];
