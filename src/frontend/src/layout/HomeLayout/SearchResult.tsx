52b4225d68df4e979084cb7e8be6c07dc05d8cee
kaizo-sudo <sapladamanda14@gmail.com>
2024-10-20 17:21:51 +0000

    feat: searchresult ui

--- src/frontend/src/layout/HomeLayout/SearchResult.tsx
+++ src/frontend/src/layout/HomeLayout/SearchResult.tsx
@@ -1,19 +1,64 @@
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
-
 
 const ResultsPage = () => {
     const [searchParams] = useSearchParams();
-    const query = searchParams.get("search") || "";
+    const query = searchParams.get("query") || "";
+    const [tutorials, setTutorials] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [organizations, setOrganizations] = useState([]);
 
     return (
-        <div className="flex flex-col gap-y-2">
-            <h1 className="text-2xl font-bold pl-2">Your Search for : 
-                "<span
-                  className="font-medium text-lg"
-                 >
-                {query}
-                </span>"
+        <div className="h-full flex flex-col gap-y-4 p-4">
+            <h1 className="text-2xl font-bold">
+                Your Search for:
+                <span className="font-medium text-lg"> "{query}" </span>
             </h1>
+
+            {tutorials.length > 0 && (
+                <div className="flex flex-col border p-2 gap-y-4 rounded-xl shadow">
+                    <h2 className="text-xl font-semibold">Tutorials</h2>
+                    {tutorials.map((tutorial, index) => (
+                        <div className={`shadow p-4 rounded-xl cursor-pointer
+                          ${index % 2 === 0 ? "bg-slate-100" : "bg-white"}
+                        `}>
+                            {/* {tutorial.title} */}
+                        </div>
+                    ))}
+                </div>
+            )}
+
+            {users.length > 0 && (
+                <div className="flex flex-col border p-2 gap-y-4 rounded-xl shadow">
+                    <h2 className="text-xl font-semibold">Users</h2>
+                    {users.map((user, index) => (
+                        <div className={`shadow p-4 rounded-xl cursor-pointer
+                      ${index % 2 === 0 ? "bg-slate-100" : "bg-white"}
+                    `}>
+                            {/* {user.title} */}
+                        </div>
+                    ))}
+                </div>
+            )}
+
+            {organizations.length > 0 && (
+                <div className="flex flex-col border p-2 gap-y-4 rounded-xl shadow">
+                    <h2 className="text-xl font-semibold">Organization</h2>
+                    {organizations.map((organization, index) => (
+                        <div className={`shadow p-4 rounded-xl cursor-pointer
+                      ${index % 2 === 0? "bg-slate-100" : "bg-white"}
+                    `}>
+                            {/* {organization.title} */}
+                        </div>
+                    ))}
+                </div>
+            )}
+
+            {tutorials.length === 0 && users.length === 0 && organizations.length === 0 && (
+                <div className="h-24 flex items-center justify-center">
+                  <p className="text-gray-500">No results found for: "{query}"</p>
+                </div>
+            )}
         </div>
     );
 };
